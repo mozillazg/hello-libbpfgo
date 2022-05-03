698cce1da01bba5a741932f946ff92a39ef8877a
mozillazg <mozillazg101@gmail.com>
2022-05-03 18:31:44 +0800

    add 03-ring-buf

---
+++ 03-ring-buf/main.bpf.c
@@ -0,0 +1,30 @@
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include "common.h"
+
+/* BPF ringbuf map */
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024 /* 256 KB */);
+} events SEC(".maps");
+
+SEC("kprobe/do_sys_openat2")
+int kprobe__do_sys_openat2(struct pt_regs *ctx)
+{
+		struct event *e;
+		e = bpf_ringbuf_reserve(&events, sizeof(*e), 0);
+		if (!e) {
+			return 0;
+		}
+
+		e->pid = bpf_get_current_pid_tgid() >> 32;
+		bpf_probe_read(&e->filename, sizeof(e->filename), PT_REGS_PARM2(ctx));
+
+		bpf_ringbuf_submit(e, 0);
+
+		return 0;
+}
+
+char _license[] SEC("license") = "GPL";
