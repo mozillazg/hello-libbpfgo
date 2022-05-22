8a83843bc04650ca1c1744d09483739fb83e11ae
mozillazg <mozillazg101@gmail.com>
2022-05-22 16:17:43 +0800

    add 10-map-map-type-array

---
+++ 10-map-map-type-array/common.h
@@ -0,0 +1,9 @@
+// https://elixir.bootlin.com/linux/v5.13/source/include/linux/sched.h#L215
+#define TASK_COMM_LEN			16
+
+struct event_t {
+    u32 pid;
+
+    char comm[TASK_COMM_LEN];
+    char file[256];
+};
