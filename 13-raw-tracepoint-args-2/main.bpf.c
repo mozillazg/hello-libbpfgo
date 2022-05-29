2fc31f449642e4d52d949aeb9bfef7aa621ffa38
mozillazg <mozillazg101@gmail.com>
2022-05-29 10:54:28 +0800

    add 13-raw-tracepoint-args-2

---
+++ 13-raw-tracepoint-args-2/main.bpf.c
@@ -0,0 +1,22 @@
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+// include/trace/events/sched.h
+SEC("raw_tracepoint/sched_switch")
+int raw_tracepoint__sched__sched_switch(struct bpf_raw_tracepoint_args *ctx)
+{
+    struct task_struct *prev_task = (struct task_struct *) BPF_CORE_READ(ctx, args[1]);
+    struct task_struct *next_task = (struct task_struct *) BPF_CORE_READ(ctx, args[2]);
+
+    u32 prev_pid = BPF_CORE_READ(prev_task, tgid);
+    u32 next_pid = BPF_CORE_READ(next_task, tgid);
+
+    char fmt[] = "sched_switch %d -> %d\n";
+    bpf_trace_printk(fmt, sizeof(fmt), prev_pid, next_pid);
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
---
+++ 13-raw-tracepoint-args-2/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	bpf "github.com/aquasecurity/libbpfgo"
+)
+
+func main() {
+	bpfModule, err := bpf.NewModuleFromFile("main.bpf.o")
+	if err != nil {
+		panic(err)
+	}
+	defer bpfModule.Close()
+
+	if err := bpfModule.BPFLoadObject(); err != nil {
+		panic(err)
+	}
+	prog, err := bpfModule.GetProgram("raw_tracepoint__sched__sched_switch")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := prog.AttachRawTracepoint("sched_switch"); err != nil {
+		panic(err)
+	}
+
+	for {
+		fmt.Println("Waiting...")
+		time.Sleep(10 * time.Second)
+	}
+}
