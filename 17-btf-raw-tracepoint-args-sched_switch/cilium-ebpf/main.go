2c628832ea31254b79f8cfaf8170cdbe304fc226
mozillazg <mozillazg101@gmail.com>
2023-08-20 04:20:44 +0000

    17: add cilium/ebpf example

---
+++ 17-btf-raw-tracepoint-args-sched_switch/cilium-ebpf/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS bpf ../main.bpf.c -- -I../ -I../output
+
+func main() {
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatal(err)
+	}
+	defer objs.Close()
+
+	tp, err := link.AttachTracing(link.TracingOptions{
+		Program:    objs.BtfRawTracepointSchedSwitch,
+		AttachType: ebpf.AttachTraceRawTp,
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer tp.Close()
+
+	log.Println("Waiting for events...")
+	time.Sleep(time.Minute * 1024)
+
+}
