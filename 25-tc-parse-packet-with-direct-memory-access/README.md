0857c17642c08f49561ff9cb981ae7c30b95e7ef
mozillazg <mozillazg101@gmail.com>
2023-11-19 12:57:36 +0000

    25: send data to userspace via bpf_perf_event_output

--- 25-tc-parse-packet-with-direct-memory-access/README.md
+++ 25-tc-parse-packet-with-direct-memory-access/README.md
@@ -18,5 +18,4 @@ $ printf 'HTTP/1.1 200 OK\nContent-Length: 0\n\n' |nc -l 9090 &
 
 $ curl http://127.0.0.1:9090
 
-$ make cat
 ```
--- 25-tc-parse-packet-with-direct-memory-access/main.bpf.c
+++ 25-tc-parse-packet-with-direct-memory-access/main.bpf.c
@@ -9,20 +9,17 @@
 #define ETH_HLEN 14 /* Total octets in header.	 */
 
 #define TC_ACT_UNSPEC -1
-#define TC_ACT_SHOT 2
-#define TC_ACT_SHOT 2
+#define TC_ACT_OK      0
+#define TC_ACT_SHOT    2
 
-#define DATA_LEN 1024
-struct payload_t {
-    char data[DATA_LEN];
+struct event_t {
 };
 
 struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __type(key, u32);
-    __type(value, struct payload_t);
-    __uint(max_entries, 1);
-} tmp_map SEC(".maps");
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(key_size, sizeof(u32));
+    __uint(value_size, sizeof(u32));
+} events SEC(".maps");
 
 SEC("tc")
 int handle_ingress(struct __sk_buff *skb) {
@@ -31,39 +28,47 @@ int handle_ingress(struct __sk_buff *skb) {
 
     struct iphdr *ip_hdr = data + ETH_HLEN;
     if ((void *)ip_hdr + sizeof(struct iphdr) > data_end) {
-        return TC_ACT_UNSPEC;
+        return TC_ACT_OK;
     }
     if (ip_hdr->protocol != IPPROTO_TCP) { // not tcp
-        return TC_ACT_UNSPEC;
+        return TC_ACT_OK;
     }
 
     struct tcphdr *tcp_hdr = (void *)ip_hdr + sizeof(struct iphdr);
     if ((void *)tcp_hdr + sizeof(struct tcphdr) > data_end) {
-        return TC_ACT_UNSPEC;
+        return TC_ACT_OK;
     }
     if (tcp_hdr->dest != bpf_htons(9090)) // not 9090 port
-        return TC_ACT_UNSPEC;
-    if (tcp_hdr->psh == 0) // no payload
-        return TC_ACT_UNSPEC;
+        return TC_ACT_OK;
+    // if (tcp_hdr->psh == 0) // no payload
+    //     return TC_ACT_OK;
+
+    struct event_t event = {};
+
+    u64 flags = BPF_F_CURRENT_CPU;
+    u64 save_size = (u64)(skb->len);
+    flags |= save_size << 32;
+    bpf_perf_event_output(skb, &events, flags, &event, sizeof(event));
 
     // parse tcp payload
-    char *raw_payload = (void *)tcp_hdr + tcp_hdr->doff * 4;;
-    unsigned raw_payload_size = bpf_htons(ip_hdr->tot_len) - (tcp_hdr->doff * 4) - sizeof(struct iphdr);
-    if ((void *)raw_payload + raw_payload_size > data_end) {
-        return TC_ACT_UNSPEC;
-    }
+    // char *raw_payload = (void *)tcp_hdr + tcp_hdr->doff * 4;;
+    // unsigned raw_payload_size = bpf_htons(ip_hdr->tot_len) - (tcp_hdr->doff * 4) - sizeof(struct iphdr);
+    // if ((void *)raw_payload + raw_payload_size > data_end) {
+    //     return TC_ACT_OK;
+    // }
 
-    u32 id = 0;
-    struct payload_t *payload = bpf_map_lookup_elem(&tmp_map, &id);
-    if (!payload)
-        return TC_ACT_UNSPEC;
+    // u32 id = 0;
+    // struct payload_t *payload = bpf_map_lookup_elem(&tmp_map, &id);
+    // if (!payload)
+    //     return TC_ACT_OK;
 
-    bpf_probe_read_kernel(&payload->data, sizeof(payload->data), raw_payload);
+    // __builtin_memset(payload->data, 0, sizeof(payload->data));
+    // bpf_probe_read_kernel(&payload->data, sizeof(payload->data), raw_payload);
 
-    char fmt[] = "payload:\n%s";
-    bpf_trace_printk(fmt, sizeof(fmt), payload->data);
+    // char fmt[] = "payload:\n%s";
+    // bpf_trace_printk(fmt, sizeof(fmt), payload->data);
 
-    return TC_ACT_UNSPEC;
+    return TC_ACT_OK;
 }
 
 char _license[] SEC("license") = "GPL";
